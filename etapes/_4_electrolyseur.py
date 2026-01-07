0d61499447c6e6d9cb3050093c00fd3b44984f93
lisasko <lisa.escaron@gmail.com>
2026-01-07 14:33:31 +0100

    maj electrolyseur

--- etapes/_4_electrolyseur.py
+++ etapes/_4_electrolyseur.py
@@ -1,19 +1,19 @@
 """
-Paramètres et hypothèses sourcées pour l'électrolyseur, puis fonctions de calcul des émissions.
+Paramètres et hypothèses sourcées pour l'électrolyseur, puis fonctions de calcul de la consommation électrique.
 
 ⚠⚠⚠⚠
 La méthode appliquée permet de calculer les performances d'un électrolyseur Alcalin ou PEM 
-avec des  technologies de 2020 et  supposées être disponibles en 2030.
+avec des technologies de 2020 et supposées être disponibles en 2030.
 
 Deux autres méthodes d'électrolyse sont en cours d'étude (SOEC = Solide Oxide Electrolysis Cell
 et AEM = Anion Exchange Membrane) mais ne sont pas encore intégrées dans ce modèle.
 """
 
 ### Coordination sur les unités utilisées :
-# Energie en kWh
+# Energie (électricité) en kWh
 # Masse en Tonnes
 # Emissions en kgCO2eq/an
-# Chaleur en MJ
+# Chaleur en MJ (Conversion : 1 kWh = 3.6 MJ)
 ###
 """Paramètres et hypothèses sourcées pour l'électrolyseur, puis fonctions de calcul des émissions.
 
@@ -32,16 +32,17 @@ et AEM = Anion Exchange Membrane) mais ne sont pas encore intégrées dans ce mo
 # Chaleur en MJ
 ###
 
+
 ###############################################################
 # Stockage des paramètres avec les hypothèses sourcées
 ###############################################################
 
 """
 Remarques sur les hypothèses : 
-- L'énergie « stackée » Estack est l'énergie électrique totale en kWh / kgH2 (électrolyse, mais aussi 
-    fonctionnement global de l'unité de production) nécessaire à la fabrication la distribution de l'H2, 
-    sans prise en compte de la perte en ligne amont.
-- Nous n'utiliserons que la valeur stackée de l'électrolyse alcaline car c'est celle choisie 
+- L'énergie « stackée » Estack est l'énergie électrique totale en kWh / kgH2 (comprend l'électrolyse, 
+    mais aussi le fonctionnement global de l'unité de production) nécessaire à la fabrication la 
+    distribution de l'H2, sans prise en compte de la perte en ligne amont.
+- Nous n'utiliserons que la valeur stackée de l'électrolyse alcaline car c'est la technologie choisie 
     par E-CHO
 
 """
@@ -50,30 +51,33 @@ Remarques sur les hypothèses :
 
 param_electrolyseur_alcalin = {
     "efficacité_electrolyseur": 0.7,            # Efficacité de l'électrolyseur (Source : ADEME)
-    "consommation_electricite_stack": 56,     # Consommation d'électricité stackée (kWh/kg H2) (Source : ADEME) 
+    "consommation_electricite_stack": 56000,    # Consommation d'électricité stackée (kWh/tonnes H2) (Source : ADEME) 
                                                 
     "consommation_eau" : 1,                     # L/Nm3 
     "produit_alcalin" : 0,
     "pertes" : 0.979                            # Pertes en ligne : sur le réseau de distribution 
+                                                # Valeur donnée par RTE : 2.1% de pertes sur le réseau haute tension (utilisé par les industries)
 }
+
 
 # Electrolyse PEM (Proton Exchange Membrane):
 # (cette technologie pose des problèmes de fiabilité)
 
 param_electrolyseur_PEM = {
     "efficacité_electrolyseur": 0.75,           # Efficacité de l'électrolyseur PEM
-    "pertes" : 0.979                            # Pertes en ligne : sur le réseau de distribution 
+    "pertes" : 0.979                            # Pertes en ligne : sur le réseau de distribution (idem alcalin)
 }
 
 
+
 ##############################################################
-# Fonction de calcul de la consommation électrique spécifique à partir de la référence
+# Fonction de calcul de la consommation électrique spécifique à partir de la technologie de référence
 ##############################################################
 
 def consom_elec_stack(param_electrolyseur_ref, param_electrolyseur_cible):
     """
-    Calcul de la consommation électrique stackée de l'électrolyseur cible (PEM) à partir de la référence (Alcalin)
-    en ajustant (normalisation) avec les rendements respectifs.
+    Calcul de la consommation électrique stackée de l'électrolyseur cible (PEM ou autre) à partir de la 
+    référence (Alcalin) en ajustant (normalisation) avec les rendements respectifs.
     """
     consommation_stack_ref = param_electrolyseur_ref["consommation_electricite_stack"]
     rendement_ref = param_electrolyseur_ref["efficacité_electrolyseur"]
@@ -103,22 +107,24 @@ def coherence_electrolyse(besoin_H2, besoin_O2):
     else:
         return True
     
+
+
 ##############################################################
-# Fonction de calcul des émissions
+# Fonction de calcul de la consommation électrique de l'électrolyseur
 ##############################################################
 
 """
 ENTREES : 
-- Quantité de H² à produire : donnée par la gazéification et Fischer-Tropsch 
-- Quantité de 0² à produire : donnée par la gazéification
+- Quantité de H² à produire : donnée par la gazéification et Fischer-Tropsch  (en tonnes)
+- Quantité de O² à produire : donnée par la gazéification (en tonnes)
 - Paramètres de l'électrolyseur (efficacité, consommation électricité, émissions électricité) : param_electrolyseur
 
 SORTIES :
-- Consommation électrique de l'électrolyseur
+- Consommation électrique de l'électrolyseur (en kWh)
 
 """
 
-def emissions_electrolyseur(param_electrolyseur, besoin_H2_FT, besoin_O2_gazif, besoin_H2_gazif):
+def consommation_electrolyseur(param_electrolyseur, besoin_H2_FT, besoin_O2_gazif, besoin_H2_gazif):
     # Calcul du besoin total en H2 de l'électrolyseur
     besoin_H2 = besoin_H2_FT + besoin_H2_gazif 
 
