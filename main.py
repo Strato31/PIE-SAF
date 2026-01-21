b2c668bba1395d21d753ac202a8746e4ab5df464
Mar2987 <rivollet.margaux@gmail.com>
2026-01-21 12:15:09 +0100

    compression des gaz final !

--- main.py
+++ main.py
@@ -44,15 +44,20 @@ def __main__():
     masse_CO_kg = CO_gazif * 1000  # Conversion en kg
     masse_H2_kg = besoin_H2_gazif * 1000  # Conversion en kg
     masse_CO2_kg = emissions_gazif * 1000
+    masse_CO2_kg_entree = masse_seche_biomasse * gaz.gaz_params['taux_carbone'] * 1000  # Conversion en kg
     masse_O2_kg = besoin_O2_gazif * 1000  # Conversion en kg
+    masse_CO2_kg_entree = 153853000
+    masse_H2_kg = 32130000
+    masse_CO_kg = 176142000
     "Calcul de la consommation électrique de compression de l'O2 entre l'électrolyseur et FT"
     conso_compression_O2 = comp.conso_compression(masse_O2_kg, "O2", 0.8, 1, 20, 288.15)
     "Calcul de la consommation électrique de compression du CO2 capté sortie du réactuer BioTJet et amené à EM-Lacq"
     conso_compression_CO2 = comp.conso_compression(masse_CO2_kg, "CO2", 0.8, 1, 20, 288.15)
     "Calcul de la consommation électrique de compression du syngas entre la gazéification et FT"
-    conso_compression_syngas = comp.conso_compression_syngaz(masse_CO2_kg, masse_H2_kg, masse_CO_kg, 0.85, 1, 1.12, 323.15) #hypothèse flux total de gaz à ventiler
+    conso_compression_syngas = comp.conso_compression_syngaz(masse_CO2_kg_entree, masse_H2_kg, masse_CO_kg, 0.85, 1, 1.12, 323.15) #hypothèse flux total de gaz à ventiler
     conso_elec_compression = conso_compression_O2 + conso_compression_syngas + conso_compression_CO2
     consos_energies.append(conso_elec_compression)
+    print('Consommation électrique totale de compression : ', conso_elec_compression, ' kWh')
     print("---------------------------------------------------------------")
     print("Étape 6 : Calcul des émissions totales et consommations énergétiques")
     emissions_2050, emissions_2023 = energie.emissions_energie_totale(consos_energies)
@@ -63,7 +68,6 @@ def __main__():
     total_emissions_2023 = sum(emissions_2023)
     total_conso_energie = sum(consos_energies)
     total_conso_thermique = sum(consos_thermiques)
-    print(f" - Consommation d'énergie : {consos_energies}")
     print(f" - Consommation électrique totale : {total_conso_energie:.2f} kWh")
     print(f" - Consommation thermique totale : {total_conso_thermique:.2f} MJ")
     print(f" - Émissions totales en 2050 : {total_emissions_2050:.2f} gCO2e")
